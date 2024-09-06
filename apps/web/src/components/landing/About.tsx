60d6a3d58fea95e003a987cdfb4891dc47da770d
joshuasilva414 <joshuasilva414@gmail.com>
2024-09-06 01:02:19 -0500

    Add: about section

--- apps/web/src/components/landing/About.tsx
+++ apps/web/src/components/landing/About.tsx
@@ -1,29 +1,37 @@
 import Balancer from "react-wrap-balancer";
 import Image from "next/image";
-import D1 from "../../../public/img/landing/d1.svg";
-import D2 from "../../../public/img/landing/d2.svg";
-import D3 from "../../../public/img/landing/d3.svg";
-import D4 from "../../../public/img/landing/d4.svg";
-import Dino_Coding from "../../../public/img/landing/dinos_coding.png";
 export default function About() {
-	const d1_stylesheet = {
-		width: "25rem",
-		height: "auto",
-		sm: "width: 30rem",
-	};
 	return (
 		<section
 			className="bg-forest-ground flex min-h-screen w-full items-center justify-center"
 			id="About"
 		>
-			<div className="bg-about flex w-full flex-col items-center justify-center">
-				<h1 className="text-center text-4xl font-black md:text-5xl">
-					About Section
-				</h1>
-				<h3 className="px-4 text-center text-lg font-bold md:px-0 md:text-2xl">
-					Introduce the hackathon and its purpose! Make it sound
-					enticing
-				</h3>
+			<div className="bg-about w-3/4flex-col mx-auto flex flex-col items-center justify-center px-16 py-20 font-roca">
+				<div className="p-8">
+					<h1 className="text-center text-3xl font-black md:text-5xl">
+						About Us
+					</h1>
+					<h3 className="max-w-[40ch] text-wrap text-center text-lg font-bold md:px-0 md:text-2xl">
+						The Rowdy Datathon is a free, weekend-long, overnight
+						hackathon focused on data science wehre students can
+						network, learn about data science, code, and
+						collaborate!
+					</h3>
+				</div>
+				{/* Some bears here */}
+				<div>Bears here</div>
+				<div className="p-8">
+					<h1 className="text-center text-3xl font-black md:text-5xl">
+						Who Can Attend?
+					</h1>
+					<h3 className="max-w-[40ch] text-center text-lg font-bold md:px-0 md:text-2xl">
+						We're thrilled to invite hackers of all skill levels,
+						backgrounds, and disciplines! Whether you're a seasoned
+						data analyst with countless datasets under your belt or
+						a newcomer eager to dive into your first one, Datathon
+						has a spot just for you
+					</h3>
+				</div>
 			</div>
 		</section>
 	);
